3a1e19674cb0637a1bde263451c489bb4cbe0c6d
Uģis Ozols <ugis.ozolss@gmail.com>
2012-06-16 14:46:10 +0300

    not much to say here ... just modeling.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,13 @@
+# encoding: utf-8
+
 class Parser
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+  end
+  
+  def content
+    File.open(@file).read
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,7 +1,20 @@
+# encoding: utf-8
+
 require_relative "../lib/parser"
 
 describe Parser do
-  it "yay" do
-    true
+  it "expects input file" do
+    Parser.new("blabla").should_not raise_error(ArgumentError)
+  end
+
+  describe ".content" do
+    before do
+      @file = File.expand_path("../fixtures/bankas_viss.px", __FILE__)
+    end
+
+    it "returns passed in file content" do
+      parser = Parser.new(@file)
+      parser.content.should =~ /Hey from px file/
+    end
   end
 end
