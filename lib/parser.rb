6dd6556023ac1a39d06b2b31c5dc2e8d604493e8
Uģis Ozols <ugis.ozolss@gmail.com>
2012-06-16 17:38:34 +0300

    alfa version of parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -11,6 +11,58 @@ class Parser
   end
 
   def header
-    content.match('VALUES')
+    "#{header_first}, #{header_rest}"    
+  end
+
+  def data
+    r = []
+    data_first.each_with_index do |data, idx| 
+      r << "#{data_first[idx]}, #{data_get[idx].strip.gsub(" ", ", ")}"
+    end
+    r
+  end
+
+  def to_csv
+    puts header
+    puts data
+  end
+
+  private
+
+  def data_first(val = "")
+    values[0].split('=')[1].gsub('"', "").split(",")
+  end
+  
+  def data_get
+    content.split("DATA=")[1].gsub(";", "").split("\r\n").reject {|x| x.empty?}
+  end
+
+  def values
+    x = content.gsub("\n", "").gsub("\r", "")
+    x = x.split(";")
+    values = []
+    x.each do |line|
+      values << line if line =~ /VALUES/
+    end
+    values 
+  end
+
+  def header_first
+    values[0].split('"')[1]
+  end
+
+  def header_rest
+    if values[1..-1].count >= 1
+      z = []
+      values[1].split("=")[1].split(",").each do |result|
+        values[-1].split("=")[1].split(",").each_with_index do |result2, idx|
+          z << "#{result.gsub('"', "")} #{result2.gsub('"', "")}"
+        end
+      end 
+      z.join(", ") 
+    end
   end
 end
+
+#p = Parser.new(File.expand_path("../../spec/fixtures/bankas_viss.px", __FILE__))
+#p.to_csv
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,26 +2,30 @@
 require_relative "../lib/parser"
 
 describe Parser do
+  before do
+    @file = File.expand_path("../fixtures/bankas_viss.px", __FILE__)
+    @parser = Parser.new(@file)
+  end
+
   it "expects input file" do
     Parser.new("blabla").should_not raise_error(ArgumentError)
   end
 
   describe ".content" do
-    before do
-      @file = File.expand_path("../fixtures/bankas_viss.px", __FILE__)
-    end
-
     it "returns passed in file content" do
-      parser = Parser.new(@file)
-      parser.content.should =~ /Hey from px file/
+      @parser.content.should =~ /Hey from px file/
     end
+  end
 
+  describe ".header" do
     it "should return headers" do
-      parser = Parser.new(@file)
-      parser.header.should == "Gads,"
-
-      # "Piesaistītie noguldījumi kredītiestādēs ilgtermiņa, Piesaistītie noguldījumi kredītiestādēs īstermiņa, Izsniegtie kredīti kredītiestādēs ilgtermiņa, Izsniegtie kredīti kredītiestādēs īstermiņa"
+      @parser.header.should == "Gads, Piesaistītie noguldījumi kredītiestādēs ilgtermiņa, Piesaistītie noguldījumi kredītiestādēs īstermiņa, Izsniegtie kredīti kredītiestādēs ilgtermiņa, Izsniegtie kredīti kredītiestādēs īstermiņa"
     end
+  end
 
+  describe ".data" do
+    it "returns data" do
+      @parser.data[0].should eq("1993, 58.7, 34.8, 39.3, 86")
+    end
   end
 end
